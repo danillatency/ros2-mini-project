0b42c6424f0e3eb69d3d813b7699b2a941cbd98f
danil <danillatency@gmail.com>
2025-08-13 16:16:03 +0300

    feat: add left wheels controller, add services for wheels controllers

--- src/making_delivery/include/making_delivery/left_wheels.hpp
+++ src/making_delivery/include/making_delivery/left_wheels.hpp
@@ -5,17 +5,18 @@
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/pose_array.hpp>
 #include <ros_gz_interfaces/msg/entity_wrench.hpp>
+#include <making_delivery/srv/start_left_wheels.hpp>
 
 using namespace std::chrono_literals;
 
 
 class LeftWheels : public rclcpp::Node {
 public:
-    bool torque = true;
     // rclcpp::TimerBase::SharedPtr timerAddTorque;
     std::unique_ptr<tf2_ros::Buffer> tfBuffer;
     std::shared_ptr<tf2_ros::TransformListener> tfListener;
     rclcpp::Publisher<ros_gz_interfaces::msg::EntityWrench>::SharedPtr persistentForcePublisher;
+    rclcpp::Service<making_delivery::srv::StartLeftWheels>::SharedPtr startServer;
 
     LeftWheels() : Node("left_wheels") {
         // this->timerAddTorque = this->create_wall_timer(50ms, std::bind(&LeftWheels::addTorque, this));
@@ -25,11 +26,33 @@ public:
         this->persistentForcePublisher = this->create_publisher<ros_gz_interfaces::msg::EntityWrench>(
             "/world/" + this->get_parameter("world_name").as_string() + "/wrench/persistent",
             rclcpp::QoS(rclcpp::KeepLast(10)).reliable().durability_volatile());
+        this->startServer = this->create_service<making_delivery::srv::StartLeftWheels>(
+            "/start_left_wheels", std::bind(&LeftWheels::start, this, std::placeholders::_1, std::placeholders::_2),
+            rclcpp::QoS(rclcpp::KeepLast(10)).reliable().durability_volatile());
     }
 
     void addTorque() {
         geometry_msgs::msg::TransformStamped leftWheelTransformation;
-        leftWheelTransformation = this->tfBuffer->lookupTransform("base_link", "left_front_wheel",  tf2::TimePointZero);
+        leftWheelTransformation = this->tfBuffer->lookupTransform("base_link", "left_front_wheel", tf2::TimePointZero);
         RCLCPP_INFO(this->get_logger(), std::to_string(leftWheelTransformation.transform.rotation.z).c_str());
+    }
+
+    void start(const making_delivery::srv::StartLeftWheels::Request::SharedPtr request,
+        const making_delivery::srv::StartLeftWheels::Response::SharedPtr response) {
+        auto message = ros_gz_interfaces::msg::EntityWrench();
+        message.entity.type = ros_gz_interfaces::msg::Entity().LINK;
+        message.wrench.force.x = 0;
+        message.wrench.force.y = 0;
+        message.wrench.force.z = 0;
+        message.wrench.torque.x = 0;
+        message.wrench.torque.y = 0;
+        message.wrench.torque.z = 200;
+        message.entity.name = request->frame_id + "::left_front_wheel_link";
+        this->persistentForcePublisher->publish(message);
+        message.entity.name = request->frame_id + "::left_middle_wheel_link";
+        this->persistentForcePublisher->publish(message);
+        message.entity.name = request->frame_id + "::left_back_wheel_link";
+        this->persistentForcePublisher->publish(message);
+        response->started = true;
     }
 };
